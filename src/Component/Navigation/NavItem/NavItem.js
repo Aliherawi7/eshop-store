8fb48d03ca8450703e2e49519ca7a519a2d4e1a8
Aliherawi <ALiherawi7@gmail.com>
2022-06-26 08:12:13 +0430

    font styles changed

--- src/Component/Navigation/NavItem/NavItem.js
+++ src/Component/Navigation/NavItem/NavItem.js
@@ -5,12 +5,14 @@ import "./NavItem.css"
 const NavItem = (props) =>{
     const isActive = props.link === (window.location.pathname)
     const active = isActive ? "nav-item active":"nav-item";
+    const underline = {
+        
+    }
     return (
         <li className={active} >
             <Link to={props.link} onClick={props.click}>
                 {props.children}
             </Link>
-            <div className="underline" style={{transform: isActive ? "scaleX(1)":"scaleX(0)"}}></div>
         </li>
     )
 }
