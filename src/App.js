628f51de90c4c59c3b4c31f72d6be9861613bda2
Aliherawi <ALiherawi7@gmail.com>
2022-06-29 15:01:45 +0430

    Reac lazy loading added with suspense loading

--- src/App.js
+++ src/App.js
@@ -1,35 +1,39 @@
 import './App.css';
-import React,{useState} from 'react'
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+import React, { useState, Suspense } from 'react'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Layout from './Component/Layout/Layout';
-import Home from './Component/Home/Home';
-import Store from "./Component/Store/Store"
-import Login from "./Component/Authentication/Login/Login"
-import Signup from './Component/Authentication/signup/signup';
-import ProductDetails from './Component/Store/ProductDetails/ProductDetails'
-import NotFound from './Component/Pages/NotFoundPage/NotFound'
-import SearchPage from './Component/Pages/SearchPage/SearchPage'
-import Checkout from './Component/Store/Checkout/Checkout'
-import About from './Component/Pages/About/About';
+import Loading from './Component/UI/Loading/Loading';
+
+const Home = React.lazy(() => import('./Component/Home/Home'));
+const Store = React.lazy(() => import("./Component/Store/Store"));
+const Login = React.lazy(() => import("./Component/Authentication/Login/Login"));
+const Signup = React.lazy(() => import('./Component/Authentication/signup/signup'));
+const ProductDetails = React.lazy(() => import('./Component/Store/ProductDetails/ProductDetails'));
+const NotFound = React.lazy(() => import('./Component/Pages/NotFoundPage/NotFound'));
+const SearchPage = React.lazy(() => import('./Component/Pages/SearchPage/SearchPage'));
+const Checkout = React.lazy(() => import('./Component/Store/Checkout/Checkout'));
+const About = React.lazy(() => import('./Component/Pages/About/About'));
 function App() {
-  const [authState, authSetstate] = useState({auth:false})
+  const [authState, authSetstate] = useState({ auth: false })
   return (
-    <Router>
-      <Layout>
-        <Routes>
-          <Route path="/" element={ <Home />} />
-          <Route path="/store" element={ <Store />} />
-          <Route path="/account" element={ <Login />} />
-          <Route path="/login" element={ <Login />} />
-          <Route path="/signup" element={ <Signup />} />
-          <Route path="/store/productdetails/:id" element={ <ProductDetails />} />
-          <Route path="/search/:id"  element={<SearchPage/>}/>
-          <Route path="/checkout" element={<Checkout />} />
-          <Route path="/about" element={<About />} />
-          <Route path="*" element={<NotFound size="large"/>}/>
-        </Routes>
-      </Layout>
-    </Router>
+    <Suspense fallback={<Loading />}>
+      <Router>
+        <Layout>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/store" element={<Store />} />
+            <Route path="/account" element={<Login />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/store/productdetails/:id" element={<ProductDetails />} />
+            <Route path="/search/:id" element={<SearchPage />} />
+            <Route path="/checkout" element={<Checkout />} />
+            <Route path="/about" element={<About />} />
+            <Route path="*" element={<NotFound size="large" />} />
+          </Routes>
+        </Layout>
+      </Router>
+    </Suspense>
   );
 }
 
