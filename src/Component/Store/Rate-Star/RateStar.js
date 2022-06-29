7a51bfe6c277b3b6c95ed141762002d09591ab47
Aliherawi <ALiherawi7@gmail.com>
2022-06-29 14:27:17 +0430

    inline styles removed, styles added in RateStar.css

--- src/Component/Store/Rate-Star/RateStar.js
+++ src/Component/Store/Rate-Star/RateStar.js
@@ -4,7 +4,7 @@ function RateStar({ size, rate, type }) {
     return (
         <div className={`product-rate  star-rate-${size}`}>
             {Array(rate).fill().map((item)=>{
-                return <i key={Math.random()} className="bi bi-star-fill" style={{padding:'3px',color:'#e7d427ee'}}></i>
+                return <i key={Math.random()} className="start-logo bi bi-star-fill" ></i>
             })}
             <span style={{paddingLeft:'5px'}}>{type}</span>
         </div>
