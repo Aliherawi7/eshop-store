817d29a502cc3c20311427273c26d17495c274a1
Aliherawi <ALiherawi7@gmail.com>
2022-06-29 17:11:13 +0430

    Transition module removed instead simple css animation used

--- src/Component/Home/Home.js
+++ src/Component/Home/Home.js
@@ -10,46 +10,41 @@ import { useNavigate } from 'react-router-dom'
 const Home = () => {
     const navigate = useNavigate();
     return (
-        <Transition timeout={500} in={true} appear>
-            {(status) => (
-                <div className={`home home-${status}`}>
-                    <Slider />
-                    <div className='most-ordered'>
-                        <div className='ordered-item'>
-                            <img src={Products[8].image} />
-                            <div className='order-info'>
-                                <h1>{Products[8].name}</h1>
-                                <Button click={()=>navigate('/store/productdetails/' + 9)}>SHOP NOW</Button>
-                            </div>
-                        </div>
-                        <div className='ordered-item'>
-                            <img src={Products[7].image} />
-                            <div className='order-info'>
-                                <h1>{Products[7].name}</h1>
-                                <Button click={()=>navigate('/store/productdetails/' + 8)}>SHOP NOW</Button>
-                            </div>
-                        </div>
-                        <div className='ordered-item'>
-                            <img src={Products[6].image} />
-                            <div className='order-info'>
-                                <h1>{Products[6].name}</h1>
-                                <Button click={()=>navigate('/store/productdetails/' + 7)}>SHOP NOW</Button>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="features">
-                        <h2>eShop Features</h2>
-                        <div className="container">
-                            <Feature iconName="bi bi-cart-check-fill" featureTitle="Free Shipping" featureText="Order Over 90Km" />
-                            <Feature iconName="bi bi-display" featureTitle="30 Days Return" featureText="for good issues" />
-                            <Feature iconName="bi bi-shield-lock-fill" featureTitle="secure payments" featureText="100% safe and secure" />
-                            <Feature iconName="bi bi-telephone-inbound-fill" featureTitle="24/7 support" featureText="24 hours support" />
-                        </div>
+        <div className={`home home-entering`}>
+            <Slider />
+            <div className='most-ordered'>
+                <div className='ordered-item'>
+                    <img src={Products[8].image} />
+                    <div className='order-info'>
+                        <h1>{Products[8].name}</h1>
+                        <Button click={() => navigate('/store/productdetails/' + 9)}>SHOP NOW</Button>
                     </div>
                 </div>
-            )}
-
-        </Transition>
+                <div className='ordered-item'>
+                    <img src={Products[7].image} />
+                    <div className='order-info'>
+                        <h1>{Products[7].name}</h1>
+                        <Button click={() => navigate('/store/productdetails/' + 8)}>SHOP NOW</Button>
+                    </div>
+                </div>
+                <div className='ordered-item'>
+                    <img src={Products[6].image} />
+                    <div className='order-info'>
+                        <h1>{Products[6].name}</h1>
+                        <Button click={() => navigate('/store/productdetails/' + 7)}>SHOP NOW</Button>
+                    </div>
+                </div>
+            </div>
+            <div className="features">
+                <h2>eShop Features</h2>
+                <div className="container">
+                    <Feature iconName="bi bi-cart-check-fill" featureTitle="Free Shipping" featureText="Order Over 90Km" />
+                    <Feature iconName="bi bi-display" featureTitle="30 Days Return" featureText="for good issues" />
+                    <Feature iconName="bi bi-shield-lock-fill" featureTitle="secure payments" featureText="100% safe and secure" />
+                    <Feature iconName="bi bi-telephone-inbound-fill" featureTitle="24/7 support" featureText="24 hours support" />
+                </div>
+            </div>
+        </div>
     )
 }
 
