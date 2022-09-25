4e19b3d23dbc4c016b3bd1bd784f279fa2f8ece9
Aliherawi7 <Aliherawi7@gmail.com>
2022-09-25 16:47:04 -0700

    panel menu and panel info component created

--- src/Component/Admin-panel/AdminPanel.js
+++ src/Component/Admin-panel/AdminPanel.js
@@ -1,16 +1,34 @@
 import React from 'react'
 import "./AdminPanel.css"
+import { useStateValue } from "../../StateProvider"
+import { Link } from 'react-router-dom';
 
 function AdminPanel() {
+  const [state, dispatch] = useStateValue();
+  console.log(state)
   return (
     <div className='admin-panel'>
-        <div className='admin-menu'>
-          <div className='account-info'>account name</div>
-          <span className='text-menu'>menu</span>
+      <div className='admin-menu'>
+        <div className='account-info'>
+          <div>
+            <img src='' alt='' />
+            <h3>{state.userInfo?.name + " " + state.userInfo?.lastName}</h3>
+            <p>{state.userInfo?.email}</p>
+          </div>
+          <Link to="/"><i className='bi bi-gear-fill' style={{"--i":"#2f3142"}}></i></Link>
         </div>
-        <div className='info-panel'>
-          right side
-        </div>
+        <span className='text-menu'>menu</span>
+        <ul className='menu-list'>
+          <li style={{ "--i": "#6969d9" }}><i className='bi bi-house-door-fill' ></i> Dashboard</li>
+          <li style={{ "--i": "#32a7e1" }}><i className='bi bi-box' ></i> Products</li>
+          <li style={{ "--i": "#b44ac6" }}><i className='bi bi-boxes' ></i> Categories</li>
+          <li style={{ "--i": "#26ad80" }}><i className='bi bi-credit-card-2-front-fill' ></i> Orders</li>
+          <li style={{ "--i": "#fe8907" }}><i className='bi bi-people-fill' ></i> Users</li>
+        </ul>
+      </div>
+      <div className='info-panel'>
+        right side
+      </div>
     </div>
   )
 }
