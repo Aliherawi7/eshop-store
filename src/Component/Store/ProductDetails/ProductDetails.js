aee60ec268a8cad6d02c206a3f432551e7a42332
Aliherawi7 <Aliherawi7@gmail.com>
2022-09-25 16:47:54 -0700

    addToBasket function bug fixed

--- src/Component/Store/ProductDetails/ProductDetails.js
+++ src/Component/Store/ProductDetails/ProductDetails.js
@@ -7,28 +7,45 @@ import DetailsPane from './productDetailPane/detailsPane'
 import { useStateValue } from '../../../StateProvider'
 import Products from '../../../products'
 import RateStar from '../Rate-Star/RateStar'
+import { actions } from '../../../reducer'
 
 const ProductDetails = () => {
-    const [, dispatch] = useStateValue()
+    const [{basket}, dispatch] = useStateValue()
     const [people, peopleSetState] = useState([
         { name: 'Ali', avatar: '/image/people/1.jpg', reviewText: "this is a sample test", date: "Today, 11:10 am" },
         { name: 'Maria', avatar: '/image/people/2.jpg', reviewText: "this is a sample test", date: "Today, 11:10 am" }
     ])
     const { id } = useParams()
+
     const foundProduct = Products.find((item) => {
         return item.id === id
     })
 
     const addToBasket = () => {
+        const index = basket.findIndex(item =>{
+            return item.id == id;
+        })
+        
+        if(index >=0){
+            dispatch({
+                type: actions.CHANGE_QUANTITY,
+                item:{
+                    id:id,
+                    index:index,
+                    quantity: basket[index].quantity + 1
+                }
+            })
+            return;
+        }
         dispatch({
             type: 'ADD_TO_BASKET',
             item: {
-                id: foundProduct.id,
                 name: foundProduct.name,
-                price: foundProduct.price,
                 image: foundProduct.image,
-                rate: foundProduct.rate,
-                quantity: 1
+                price: foundProduct.price,
+                rating: foundProduct.rating,
+                id: id,
+                quantity:1
             }
         })
     }
