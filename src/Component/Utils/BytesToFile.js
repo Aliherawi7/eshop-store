d5a5d4eaf1a4f392d051028ff9b467e8264e2484
Aliherawi7 <Aliherawi7@gmail.com>
2022-10-05 13:15:30 -0700

    BytesToFile tool created for converting the bytes which comming from the sever as response to the file

---
+++ src/Component/Utils/BytesToFile.js
@@ -0,0 +1,12 @@
+export function BytesToFile(data, type) {
+    let binaryString = window.atob(data);
+    let binaryLength = binaryString.length;
+    let bytes = new Uint8Array(binaryLength);
+    for (let i = 0; i < binaryLength; i++) {
+        let ascii = binaryString.charCodeAt(i);
+        bytes[i] = ascii;
+    }
+    let blob = new Blob([bytes], { type: type });
+    return window.URL.createObjectURL(blob);
+}
+ 
