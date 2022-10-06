a4c4f2b9611c4c973bb2f0e10b910f737bbc8816
Aliherawi7 <Aliherawi7@gmail.com>
2022-10-06 11:27:45 -0700

    the products page get data from the server now

--- src/Component/Store/Store.js
+++ src/Component/Store/Store.js
@@ -1,16 +1,37 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import "./Store.css"
 import Loading from '../UI/Loading/Loading'
 import Products from '../../products'
 import Product from './Product/Product'
+import { BytesToFile } from '../Utils/BytesToFile'
 
 const Store = () => {
     let producstElement;
-    if (Products) {
+    const [products, setProducts] = useState();
+    useEffect(() => {
+      const getData = ()=>{
+          fetch('http://localhost:8080/api/products').then(res =>{
+              if(res.ok){
+                  return res.json();
+              }
+          }).then(data =>{
+              console.log(data)
+              data.map(item =>{
+                  item.image = BytesToFile(item.image, {contentType: "image/png"})
+                  return item
+              })
+              setProducts(data);
+          })
+      }
+    
+      return getData();
+    }, [])
+    
+    if (products) {
         producstElement = (
             <div className="store store-entering">
                 <div className="product-list">
-                    {Products.map((item) => (
+                    {products.map((item) => (
                         <Product
                             id={item.id}
                             image={item.image}
