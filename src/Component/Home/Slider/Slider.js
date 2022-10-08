7485521236106e93a80c0ed91fadaa8d26a4097f
Aliherawi7 <Aliherawi7@gmail.com>
2022-10-08 16:12:18 -0700

    slider get its data from server now

--- src/Component/Home/Slider/Slider.js
+++ src/Component/Home/Slider/Slider.js
@@ -1,9 +1,9 @@
 import React, { useEffect, useState } from 'react'
 import "./Slider.css"
-import Products from '../../../products'
 import SliderItems from "./SliderItem"
+import { BytesToFile } from "../../Utils/BytesToFile"
 
-let counter = 0;
+let counter = 1;
 
 //for slider animation onclick
 const animations = {
@@ -11,47 +11,64 @@ const animations = {
     RIGHT_TO_LEFT: 'right-to-left',
 }
 function Slider() {
-    const [Product, setProduct] = useState({
-        component: SliderItems[counter%2],
+    let [products, setProducts] = useState([]);
+
+    const [slide, setSlide] = useState({
+        component: SliderItems[counter % 2],
         animate: animations.LEFT_TO_RIGHT
     });
     useEffect(() => {
-      
-      const interval = setInterval(()=>{
-        next();
-      }, 4000)
-      console.log("in useEffect slider")
-        return () => {
-          clearInterval(interval);
+        const getData = () => {
+            fetch('http://localhost:8080/api/products').then(res => {
+                if (res.ok) {
+                    return res.json();
+                }
+            }).then(data => {
+                const p = data.map(item => {
+                    item.image = BytesToFile(item.image)
+                    return item
+                });
+                setProducts(p)
+            })
+
         }
-    
+        getData();
+        const interval = setInterval(() => {
+            next();
+        }, 4000)
+
+        return () => {
+            clearInterval(interval);
+        }
+
     }, [])
-    
+
 
     const next = () => {
         counter++;
-        if (counter >= Products.length - 1) {
-            counter = 0;
+        if (counter >= 10) {
+            counter = 1;
         }
-        setProduct({ component: SliderItems[counter % 2], animate: animations.RIGHT_TO_LEFT })
+        setSlide({ component: SliderItems[counter % 2], animate: animations.RIGHT_TO_LEFT })
+
     }
     const prev = () => {
         counter--;
-        if (counter <= -1) {
-            counter = Products.length - 1
+        if (counter <= 0) {
+            counter = 10;
         }
-        setProduct({ component: SliderItems[counter % 2], animate: animations.LEFT_TO_RIGHT })
+        setSlide({ component: SliderItems[counter % 2], animate: animations.LEFT_TO_RIGHT })
     }
 
     return (
         <div className='slider'>
             <div className='slider-container'>
                 <i className='slide-to-left bi bi-chevron-left' onClick={prev}></i>
-                {<Product.component
-                    image={Products[counter]?.image}
-                    animate={Product?.animate}
-                    name={Products[counter]?.name}
-                    id={counter + 1}
+                {<slide.component
+                    image={products[counter]?.image}
+                    animate={slide?.animate}
+                    name={products[counter]?.name}
+                    id={products[counter]?.id}
                 />}
                 <i className='slide-to-right bi bi-chevron-right' onClick={next}></i>
             </div>
