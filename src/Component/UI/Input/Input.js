1ac44c0a262883f5485a3e492d29fdfd5c4e63ff
Aliherawi7 <Aliherawi7@gmail.com>
2022-10-08 16:20:26 -0700

    file input element added for handling file inputs

--- src/Component/UI/Input/Input.js
+++ src/Component/UI/Input/Input.js
@@ -5,32 +5,48 @@ const Input = (props) => {
     let classArray = ["input-component"]
     let containerClass = ["input-container"]
 
-    if(!props.isValid && props.isUsed){
-       console.log(props.isValid , props.isUsed,"in input")
-       containerClass.push("invalid")
+    if (!props.isValid && props.isUsed) {
+        console.log(props.isValid, props.isUsed, "in input")
+        containerClass.push("invalid")
 
-    }else if(containerClass.length > 1){
-        containerClass= ["input-container"]
+    } else if (containerClass.length > 1) {
+        containerClass = ["input-container"]
     }
-    
-    if(props.isUsed){
+
+    if (props.isUsed) {
         containerClass.push("focus")
     }
+    let element;
+    if (props.type == 'file') {
+        element = (<div className='image-input'>
+            <img src={props.value ? URL.createObjectURL(props.value) : "/image/people/avatar.png"} />
+            <input
+                type={props.type}
+                name={props.name}
+                
+                accept='image/*'
+                className={classArray.join(' ')}
+                onChange={props.change} />
+            <i className='bi bi-cloud-upload'></i>
+        </div>)
+    } else {
+        element = (
+            <div className={containerClass.join(' ')}>
+                <input
+                    type={props.type}
+                    name={props.name}
+                    value={props.value}
+                    className={classArray.join(' ')}
+                    onChange={props.change}
+                />
+                <label>{props.placeholder}</label>
+                <u></u>
+                <span className="warning-message">{props.warningMessage}</span>
+            </div>
+        )
+    }
 
-    return(
-        <div className={containerClass.join(' ')}>
-            <input 
-            type={props.type}
-            name={props.name} 
-            value={props.value} 
-            className={classArray.join(' ')}
-            onChange={props.change}
-            />
-            <label>{props.placeholder}</label>
-            <u></u>
-            <span className="warning-message">{props.warningMessage}</span>
-        </div>
-    )
+    return element
 }
 
 export default Input
