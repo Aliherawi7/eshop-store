6caa8245417c88896787f0302fab31109d3fa371
Aliherawi7 <Aliherawi7@gmail.com>
2022-10-11 14:25:15 -0700

    Bar chart and Line Chart added. [customeProduct component is available for adding new product and updating the existed product

--- src/Component/Admin-panel/AdminTools.js
+++ src/Component/Admin-panel/AdminTools.js
@@ -2,14 +2,15 @@ import React, { useState, useEffect } from 'react'
 import "./AdminTools.css"
 import Products from '../../products'
 import Button from "../UI/Button/Button"
-import AddNewProduct from './AddNewProduct'
+import CustomeProduct from './CustomeProduct'
 import AddNewCategory from './AddNewCategory'
-import BarChart from './BarChart'
+import OrderBarChart from './OrderBarChart'
 import { toast } from 'react-toastify'
 import { useStateValue } from '../../StateProvider'
 import { actions } from '../../reducer'
-import { BytesToFile } from '../Utils/BytesToFile'
+import { BytesToFile, getBlobOfFile } from '../Utils/BytesToFile'
 import Modal from '../UI/modal/Modal'
+import ProductLineChart from './ProductLineChart'
 
 export function Dashboard() {
     const [state, dispatch] = useStateValue();
@@ -51,7 +52,10 @@ export function Dashboard() {
                 </div>
 
             </div>
-            <BarChart />
+            <section className='chart-container'>
+                <OrderBarChart />
+                <ProductLineChart />
+            </section>
 
         </div>
     )
@@ -61,7 +65,8 @@ export function ProductsPanel() {
     const [state, setState] = useState(true);
     const [, dispatch] = useStateValue();
     const [products, setProducts] = useState([]);
-    const [showModal, setShowModal] = useState({show:false, productId:-1})
+    const [showModal, setShowModal] = useState({ show: false, productId: -1 })
+    let [productDetail, setProductDetail] = useState({});
     useEffect(() => {
         dispatch({
             type: actions.LOADING,
@@ -76,10 +81,10 @@ export function ProductsPanel() {
                     return res.json();
                 }
             }).then(data => {
-                data = data.map(item => {
-                    item.image = BytesToFile(item.image, "image/png");
-                    return item;
-                })
+                // data = data.map(item => {
+                //     item.image = ;
+                //     return item;
+                // })
                 console.log(data)
                 setProducts(data)
             }).catch(error => {
@@ -129,6 +134,16 @@ export function ProductsPanel() {
             }
         })
     }
+
+    const addProduct = () => {
+        setProductDetail({});
+        setState(false);
+    }
+    const editProduct = (item) => {
+        setProductDetail(item);
+        setState(false);
+    }
+
     return (state ?
         <div className='products-statistics panel-statistics fade-in'>
             <div className='products-table model-table' style={{ "--i": "#32a7e1" }}>
@@ -137,61 +152,76 @@ export function ProductsPanel() {
                         <h2>Products</h2>
                         <p>{Products.length} products</p>
                     </div>
-                    <Button btnType="white" click={() => setState(false)}>
+                    <Button btnType="white" click={addProduct}>
                         add
                         <i className='bi bi-plus-lg'></i>
                     </Button>
                 </div>
+                <Modal show={showModal.show} ModalClose={() => setShowModal({ ...showModal, show: false })}>
+                    <h2>Are you sure?</h2>
+                    <Button btnType="danger" click={() => deleteProduct(showModal.productId)}>Yes</Button>
+                    <Button btnType="success" click={() => setShowModal(!showModal)}>No</Button>
+                </Modal>
                 <table>
                     <thead>
-                        <th name='id'>ID</th>
-                        <th name="name">NAME</th>
-                        <th>CATEGORIES</th>
-                        <th>BRAND NAME</th>
-                        <th>PRICE</th>
-                        <th>INVENTORY</th>
-                        <th >STATE</th>
-                        <th name='last'>ACTIONS</th>
+                        <tr>
+                            <th name='id'>ID</th>
+                            <th name="name">NAME</th>
+                            <th>CATEGORIES</th>
+                            <th>BRAND NAME</th>
+                            <th>PRICE</th>
+                            <th>INVENTORY</th>
+                            <th >STATE</th>
+                            <th name='last'>ACTIONS</th>
+                        </tr>
                     </thead>
+                    
                     <tbody>
-                        <Modal show={showModal.show} ModalClose={() => setShowModal({...showModal, show:false})}>
-                            <h2>Are you sure?</h2>
-                            <Button btnType="danger" click={()=>deleteProduct(showModal.productId)}>Yes</Button>
-                            <Button btnType="success" click={() => setShowModal(!showModal)}>No</Button>
-                        </Modal>
-                            {products.map(product => {
-                                return (
 
+                        {products.map(product => {
+                            return (
+                                <tr key={product.id}>
+                                    <td name='id'>{product.id}</td>
+                                    <td name='name'><img src={BytesToFile(product.image, "image/png")} />{product.name?.toUpperCase()}</td>
+                                    <td >{product.category?.toUpperCase()}</td>
+                                    <td >{product.brandName?.toUpperCase()}</td>
+                                    <td name="price">${product.price}</td>
+                                    <td>{product.quantityInDepot}</td>
+                                    <td>{product.quantityInDepot > 0 ? "available" : 'not available'}</td>
+                                    <td name='last'>
+                                        <span className='action-buttons'>
+                                            <i className='bi bi-three-dots-vertical show-actions'></i>
+                                            <span className='buttons-box'>
+                                                <i className='bi bi-trash' onClick={() => setShowModal({ show: true, productId: product.id })} style={{ "--i": 'red' }}></i>
+                                                <i className='bi bi-pencil' onClick={() => editProduct(product)}></i>
+                                            </span>
+                                        </span>
+                                    </td>
+                                </tr>
 
-                                    <tr key={product.id}>
-                                        <td name='id'>{product.id}</td>
-                                        <td name='name'><img src={product.image} />{product.name?.toUpperCase()}</td>
-                                        <td >{product.category?.toUpperCase()}</td>
-                                        <td >{product.brandName?.toUpperCase()}</td>
-                                        <td name="price">${product.price}</td>
-                                        <td>{product.quantityInDepot}</td>
-                                        <td>{product.quantityInDepot > 0 ? "available" : 'not available'}</td>
-                                        <td name='last'>
-                                            <div className='action-buttons'>
-                                                <i className='bi bi-three-dots-vertical show-actions'></i>
-                                                <div className='buttons-box'>
-                                                    <i className='bi bi-trash' onClick={() => setShowModal({show: true, productId:product.id})} style={{ "--i": 'red' }}></i>
-                                                    <i className='bi bi-pencil' ></i>
-                                                </div>
-                                            </div>
-                                        </td>
-                                    </tr>
+                            )
+                        })}
 
-                                )
-                            })}
-                        
                     </tbody>
 
                 </table>
             </div>
 
         </div>
-        : <AddNewProduct back={() => setState(true)} />
+        : <CustomeProduct
+            back={() => setState(true)}
+            id={productDetail?.id}
+            name={productDetail?.name}
+            image={productDetail?.image}
+            size={productDetail?.size}
+            productionDate={productDetail?.productionDate}
+            price={productDetail?.price}
+            quantityInDepot={productDetail?.quantityInDepot}
+            description={productDetail?.description}
+            category={productDetail?.category}
+            color={productDetail?.color}
+            brandName={productDetail?.brandName}
+        />
     )
 }
 
@@ -215,11 +245,13 @@ export function CategoriesPanel() {
                     </div>
                     <table>
                         <thead>
-                            <th name='id'>ID</th>
-                            <th>NAME</th>
-                            <th>TOTAL PRODUCTS</th>
-                            <th >STATE</th>
-                            <th name='last'>ACTIONS</th>
+                            <tr>
+                                <th name='id'>ID</th>
+                                <th>NAME</th>
+                                <th>TOTAL PRODUCTS</th>
+                                <th >STATE</th>
+                                <th name='last'>ACTIONS</th>
+                            </tr>
                         </thead>
                         <tbody>
                             {categories.map(product => {
@@ -230,13 +262,13 @@ export function CategoriesPanel() {
                                         <td>{product?.amount}</td>
                                         <td>{product?.amount > 0 ? "available" : 'not available'}</td>
                                         <td name='last'>
-                                            <div className='action-buttons'>
+                                            <span className='action-buttons'>
                                                 <i className='bi bi-three-dots-vertical show-actions'></i>
-                                                <div className='buttons-box'>
+                                                <span className='buttons-box'>
                                                     <i className='bi bi-trash' style={{ "--i": 'red' }}></i>
                                                     <i className='bi bi-pencil' ></i>
-                                                </div>
-                                            </div>
+                                                </span>
+                                            </span>
                                         </td>
                                     </tr>
                                 )
@@ -309,17 +341,18 @@ export function OrdersPanel() {
                 </div>
                 <table>
                     <thead>
-                        <th name='id'>ID</th>
-                        <th >Customer</th>
-                        <th>Product</th>
-                        <th>CATEGORIES</th>
-                        <th>Quantity</th>
-                        <th>Amount</th>
-                        <th>Location</th>
-                        <th >Status</th>
-                        <th >rate</th>
-                        <th name='last'>ACTIONS</th>
-
+                        <tr>
+                            <th name='id'>ID</th>
+                            <th >Customer</th>
+                            <th>Product</th>
+                            <th>CATEGORIES</th>
+                            <th>Quantity</th>
+                            <th>Amount</th>
+                            <th>Location</th>
+                            <th >Status</th>
+                            <th >rate</th>
+                            <th name='last'>ACTIONS</th>
+                        </tr>
                     </thead>
                     <tbody>
                         {orders.map(order => {
@@ -335,13 +368,13 @@ export function OrdersPanel() {
                                     <td>{order?.amount > 0 ? "paid" : 'unpaid'}</td>
                                     <td>{order?.amount > 110 ? "4" : '5'}</td>
                                     <td name='last'>
-                                        <div className='action-buttons'>
+                                        <span className='action-buttons'>
                                             <i className='bi bi-three-dots-vertical show-actions'></i>
-                                            <div className='buttons-box'>
+                                            <span className='buttons-box'>
                                                 <i className='bi bi-trash' style={{ "--i": 'red' }}></i>
                                                 <i className='bi bi-pencil' ></i>
-                                            </div>
-                                        </div>
+                                            </span>
+                                        </span>
                                     </td>
                                 </tr>
                             )
@@ -394,16 +427,17 @@ export function UsersPanel() {
                 </div>
                 <table>
                     <thead>
-                        <th name='id'>ID</th>
-                        <th >USER NAME</th>
-                        <th>EMAIL</th>
-                        <th>TOTAL ORDERS</th>
-                        <th>TOTAL SPEDING</th>
-                        <th>LOCATION</th>
-                        <th>ROLES</th>
-                        <th >Status</th>
-                        <th name='last'>ACTIONS</th>
-
+                        <tr>
+                            <th name='id'>ID</th>
+                            <th >USER NAME</th>
+                            <th>EMAIL</th>
+                            <th>TOTAL ORDERS</th>
+                            <th>TOTAL SPEDING</th>
+                            <th>LOCATION</th>
+                            <th>ROLES</th>
+                            <th >Status</th>
+                            <th name='last'>ACTIONS</th>
+                        </tr>
                     </thead>
                     <tbody>
                         {users.map(user => {
@@ -418,13 +452,13 @@ export function UsersPanel() {
                                     <td>[ {user.roles.map(role => role + ", ")}]</td>
                                     <td>{user.active ? "active" : "deactive"}</td>
                                     <td name='last'>
-                                        <div className='action-buttons'>
+                                        <span className='action-buttons'>
                                             <i className='bi bi-three-dots-vertical show-actions'></i>
-                                            <div className='buttons-box'>
+                                            <span className='buttons-box'>
                                                 <i className='bi bi-trash' style={{ "--i": 'red' }}></i>
                                                 <i className='bi bi-pencil' ></i>
-                                            </div>
-                                        </div>
+                                            </span>
+                                        </span>
                                     </td>
                                 </tr>
                             )
