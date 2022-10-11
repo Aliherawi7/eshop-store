bbf51478503a5b21704b5606691d08667823f4bb
Aliherawi7 <Aliherawi7@gmail.com>
2022-10-11 14:31:23 -0700

    getBlobOfFIle function added

--- src/Component/Utils/BytesToFile.js
+++ src/Component/Utils/BytesToFile.js
@@ -9,4 +9,15 @@ export function BytesToFile(data, type) {
     let blob = new Blob([bytes], { type: type });
     return window.URL.createObjectURL(blob);
 }
+
+export function getBlobOfFile(data, type){
+    let binaryString = window.atob(data);
+    let binaryLength = binaryString.length;
+    let bytes = new Uint8Array(binaryLength);
+    for (let i = 0; i < binaryLength; i++) {
+        let ascii = binaryString.charCodeAt(i);
+        bytes[i] = ascii;
+    }
+    return new Blob([bytes], { type: type });
+}
  
