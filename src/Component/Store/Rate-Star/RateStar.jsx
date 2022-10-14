362a0a87f634acd69676f2720ff01b70e0e9b521
Aliherawi7 <Aliherawi7@gmail.com>
2022-10-14 09:56:02 +0430

    empty stars also appear now

--- src/Component/Store/Rate-Star/RateStar.jsx
+++ src/Component/Store/Rate-Star/RateStar.jsx
@@ -1,12 +1,16 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import './RateStar.css'
 function RateStar({ size, rate, type }) {
+    
     return (
         <div className={`product-rate  star-rate-${size}`}>
-            {Array(rate).fill().map((item)=>{
-                return <i key={Math.random()} className="start-logo bi bi-star-fill" ></i>
+            {Array(rate).fill().map(() => {
+                return <i key={Math.random()} className="star-fill bi bi-star-fill" ></i>
             })}
-            <span style={{paddingLeft:'5px'}}>{type}</span>
+            {Array(5 - rate).fill().map(() => {
+                return <i key={Math.random()} className="star-empty bi bi-star-fill" ></i>
+            })}
+            <span style={{ paddingLeft: '5px' }}>{type}</span>
         </div>
     )
 }
