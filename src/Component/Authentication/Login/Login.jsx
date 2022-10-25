03775038634df25ee5697238f5b721be7bbaee84
Aliherawi7 <Aliherawi7@gmail.com>
2022-10-25 16:41:43 +0430

    email and password validate now and the response of the server will will show to the user if there is a problem

--- src/Component/Authentication/Login/Login.jsx
+++ src/Component/Authentication/Login/Login.jsx
@@ -11,7 +11,7 @@ const Login = () => {
     const [loginInputState, setLoginInputState] = useState({
         email: {
             type: 'email',
-            placeholder: 'username/email',
+            placeholder: 'email',
             name: 'email',
             value: '',
             isValid: false,
@@ -30,6 +30,8 @@ const Login = () => {
     })
     const [state, dispatch] = useStateValue()
     const [eyeState, eyeSetState] = useState({ className: 'bi bi-eye-slash' })
+    const [signinState, setSigninState] = useState("Sign in")
+
     const changeLoginHandler = (event, item) => {
         const updatedForm = { ...loginInputState }
         const updatedElement = updatedForm[item]
@@ -83,26 +85,29 @@ const Login = () => {
     }
 
     const login = () => {
+        setSigninState("sending request...")
         const checkEmail = loginInputState.email
         const checkPassword = loginInputState.password
 
         if (checkPasswordValidation(checkEmail.value) === 'empty' || !checkEmail.isValid) {
             const holder = { ...loginInputState };
-            holder.email.warningMessage = 'fill it'
+            holder.email.warningMessage = holder.email.warningMessage.length >0 ? holder.email.warningMessage : "fill it"
             holder.email.isUsed = true
             holder.email.isValid = false
             setLoginInputState(holder)
+            setSigninState("Sign in")
             return
         }
         if (checkPasswordValidation(checkPassword.value) === 'short' || !checkPassword.isValid) {
             const holder = { ...loginInputState };
-            holder.password.warningMessage = 'fill it'
+            holder.password.warningMessage = holder.password.warningMessage.length >0 ? holder.password.warningMessage : "fill it"
             holder.password.isUsed = true
             holder.password.isValid = false
             setLoginInputState(holder)
+            setSigninState("Sign in")
             return
         }
-        
+
 
         fetch("http://localhost:8080/api/login", {
             headers: {
@@ -114,28 +119,45 @@ const Login = () => {
                 'password': checkPassword.value
             })
 
-        }).then(resposne => {
-            if (resposne.ok) {
-                return resposne.json();
+        }).then(res => {
+            if (res.status == 200) {
+                return res.json();
             }
+            
         }).then(data => {
             console.log(data)
+            if (data.error_message) {
+                console.log("in if")
+                const updatInputMessage = { ...loginInputState };
+                if (data.error_message.includes("email")) {
+                    console.log("in 2 if")
+                    updatInputMessage.email.warningMessage = data.error_message
+                    updatInputMessage.email.isValid = false
+                }else{
+                    updatInputMessage.password.warningMessage = (data.error_message +" ")+ (data.lock_expireDate? data.lock_expireDate:"")
+                    updatInputMessage.password.isValid = false
+                }
+
+                setSigninState("Sign in")
+                setLoginInputState(updatInputMessage)
+                return
+            }
             localStorage.setItem("accessToken", data?.access_token);
             localStorage.setItem("refreshToken", data?.refresh_token);
-            localStorage.setItem("name", data.userInformationDTO?.name)
-            localStorage.setItem("lastName", data.userInformationDTO?.lastName)
-            localStorage.setItem("email", data.userInformationDTO?.email)
-            localStorage.setItem("image",data.userInformationDTO?.image)
-            localStorage.setItem("roles", data.userInformationDTO?.roles)
+            localStorage.setItem("name", data?.userInformationDTO?.name)
+            localStorage.setItem("lastName", data?.userInformationDTO?.lastName)
+            localStorage.setItem("email", data?.userInformationDTO?.email)
+            localStorage.setItem("image", data?.userInformationDTO?.image)
+            localStorage.setItem("roles", data?.userInformationDTO?.roles)
             data.userInformationDTO.access_token = data?.access_token
-            console.log(localStorage.getItem('accessToken'))
+    
             dispatch({
-                type:actions.ADD_USER_INFORMATION,
+                type: actions.ADD_USER_INFORMATION,
                 item: data.userInformationDTO
             })
             navigate("/")
         }).catch(error => console.log(error))
-
+        setSigninState("Sign in")
     }
 
     let loginInputArray = []
@@ -155,7 +177,7 @@ const Login = () => {
                 <h2>Sign in</h2>
                 {loginInputArray.map((item) => {
                     return (
-                        <React.Fragment key={item.id}>
+                        <React.Fragment key={item}>
                             {(item.config.name !== 'email') ?
                                 <Button btnType={'show-password'} click={() => (eyeBtnHandler(item.name))}><i className={eyeState.className} key={item.id}></i></Button> : null}
                             <Input
@@ -172,15 +194,16 @@ const Login = () => {
                         </React.Fragment>
                     )
                 })}
-                <Button btnType={"success"} click={login}>Sign in</Button>
+                <Button btnType={"success"} click={login}>{signinState}</Button>
                 <p>By clicking the sign in you agree to the <strong>eshop</strong> Conditions of Use & Sale</p>
                 <Link to="/signup" className="already-account">create eshop account</Link>
+                <ul className='login-with-third-party'>
+                    <li style={{ "--tp": "#c70202" }}><a><i className='bi bi-google'></i></a></li>
+                    <li style={{ "--tp": "#166bf3" }}><a><i className='bi bi-facebook'></i></a></li>
+                    <li style={{ "--tp": "#01b5f2" }}><a><i className='bi bi-twitter'></i></a></li>
+                </ul>
             </form>
-            <ul className='login-with-third-party'>
-                <li style={{ "--tp": "#c70202" }}><a><i className='bi bi-google'></i></a></li>
-                <li style={{ "--tp": "#166bf3" }}><a><i className='bi bi-facebook'></i></a></li>
-                <li style={{ "--tp": "#01b5f2" }}><a><i className='bi bi-twitter'></i></a></li>
-            </ul>
+
         </div>
 
 
