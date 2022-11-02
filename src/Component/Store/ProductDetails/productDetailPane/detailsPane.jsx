740d444e7ea551f8631345d5b383ea2397f11376
Aliherawi7 <Aliherawi7@gmail.com>
2022-11-02 23:34:57 +0430

    component UI/UX completly updated

--- src/Component/Store/ProductDetails/productDetailPane/detailsPane.jsx
+++ src/Component/Store/ProductDetails/productDetailPane/detailsPane.jsx
@@ -2,24 +2,20 @@ import React, { useState } from 'react'
 import "./detailsPane.css"
 import ReviewPane from './ReviewPane/ReviewPane'
 import DataSheet from './DataSheet/DataSheet'
-import Description from './Description/Description'
 
 const DetailsPane = (props) => {
     const [state, setstate] = useState({
         description: {
             li: "tap-pane tap-active",
             content: "content-pane content-active",
-            transition: true
         },
         dataSheet: {
             li: "tap-pane ",
             content: "content-pane",
-            transition: false
         },
         reviews: {
             li: "tap-pane ",
             content: "content-pane",
-            transition: false
         }
     })
     // tab click and show active tab content
@@ -28,11 +24,9 @@ const DetailsPane = (props) => {
         for (let i in oldState) {
             oldState[i].li = "tap-pane "
             oldState[i].content = "content-pane "
-            oldState[i].transition = false
         }
         oldState[id].li = oldState[id].li + "tap-active"
         oldState[id].content = oldState[id].content + "content-active"
-        oldState[id].transition = true
         setstate(oldState)
     }
 
@@ -42,34 +36,36 @@ const DetailsPane = (props) => {
                 <li key={'description'}
                     className={state.description.li}
                     onClick={() => { active("description") }}>
-                    <i className="bi bi-receipt"></i>
-                    Descriptions
+                    Details
                 </li>
 
                 <li
                     key={'dataSheet'}
                     className={state.dataSheet.li}
                     onClick={() => { active("dataSheet") }}>
-                    <i className="bi bi-table"></i>
-                    Data Sheet
+                    Features
                 </li>
 
                 <li
                     key={'reviews'}
                     className={state.reviews.li}
                     onClick={() => { active("reviews") }}>
-                    <i className="bi bi-people"></i>
-                     Reviews</li>
+                    Reviews</li>
             </ul>
             <div className="tap-content">
                 <div className={state.description.content} >
-                    <Description transition={state.description.transition} description={props.description} />
+                    <div className={`description-content fade-in`}>
+                        <h4>Descriptions</h4>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi quod in quae, omnis, expedita ex eveniet voluptate dolores sequi qui quo eos earum ipsam laboriosam eius aliquam amet consequatur explicabo!
+                        </p>
+                    </div>
                 </div>
                 <div className={state.dataSheet.content} >
-                    <DataSheet dataSheet={props.dataSheet} transition={state.dataSheet.transition} />
+                    <DataSheet dataSheet={props.dataSheet} />
                 </div >
                 <div className={state.reviews.content}>
-                    <ReviewPane productName={props.productName} people={props.people} transition={state.reviews.transition} />
+                    <ReviewPane productId={props.dataSheet.id} />
                 </div>
             </div>
         </div>
