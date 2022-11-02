725bee2c5724d6cca80239f0f3d0dc54c6c76fc7
Aliherawi7 <Aliherawi7@gmail.com>
2022-11-02 23:22:21 +0430

    shop changed to store agian

--- src/Component/Navigation/NavItems/NavItems.jsx
+++ src/Component/Navigation/NavItems/NavItems.jsx
@@ -7,9 +7,9 @@ const NavItems =() => {
         <nav>
             <ul className="nav-items">
                 <NavItem link="/" >Home</NavItem>
-                <NavItem link="/shop" >shop</NavItem>
+                <NavItem link="/store" >store</NavItem>
                 <NavItem link="/best-sellers" >Best Sellers</NavItem>
-                <NavItem link="/customer-service" >Customer Service</NavItem>
+                <NavItem link="/customer-service" >Blog</NavItem>
                 <NavItem link="/about" >About</NavItem>
             </ul>
         </nav>
