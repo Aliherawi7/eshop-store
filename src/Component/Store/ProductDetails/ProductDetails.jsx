92f99d83fd98405421dc29a408c4db4fc98e6b8e
Aliherawi7 <Aliherawi7@gmail.com>
2022-12-05 13:53:47 +0430

    useFetch hook added, RelatedpProduct compoent added, dirty code of fetch api in useEffect removed

--- src/Component/Store/ProductDetails/ProductDetails.jsx
+++ src/Component/Store/ProductDetails/ProductDetails.jsx
@@ -10,57 +10,33 @@ import { actions } from '../../../reducer'
 import { BytesToFile } from '../../../Utils/BytesToFile'
 import { toast } from 'react-toastify'
 import Modal from '../../UI/modal/Modal'
-import Product from '../Product/Product'
 import ApiUrls from "../../../Constants/ApiUrls"
+import useFetch from '../../../Hook/useFetch'
+import RelatedProducts from '../RelatedProducts/RelatedProducts'
+import NotFound from "../../Pages/NotFoundPage/NotFound"
 
 const ProductDetails = () => {
+    const { id } = useParams()
     const [{ basket }, dispatch] = useStateValue()
     const [product, setProduct] = useState();
-    const [relatedProduct, setRelatedProduct] = useState([]);
+    const { data, error, loading } = useFetch(ApiUrls.hostName + ApiUrls.products.getProduct + id);
     const [productImage, setProductImage] = useState('');
     const [showModal, setShowModal] = useState(false)
-    const sliderRef = useRef();
-    const sliderItemRef = useRef();
-    let producstElement;
-    const { id } = useParams()
+
+    let productsElement;
+
     useEffect(() => {
         window.scrollTo(0, 0)
-
-        const getProductInfo = () => {
-            fetch(ApiUrls.hostName+ApiUrls.products.getProduct + id).then(res => {
-                if (res.ok) {
-                    return res.json();
-                }
-            }).then(data => {
-                console.log(data)
-                data.images = data.images.map((item) => {
-                    return BytesToFile(item, { contentType: "image/png" });
-                })
-                setProduct(data);
-                setProductImage(data.images[0])
-                getRalatedProducts(data.category);
-
+        if (data) {
+            const images = data?.images?.map((item) => {
+                return BytesToFile(item, { contentType: "image/png" });
             })
-        }
-        getProductInfo();
-        function getRalatedProducts(category) {
-            fetch(ApiUrls.hostName+ApiUrls.products.findProducts +'category=' + category).then(res => {
-                if (res.ok) {
-                    return res.json();
-                }
-            }).then(relatedProducts => {
-
-                relatedProducts.map(item => {
-                    item.images[0] = BytesToFile(item.images[0], { contentType: "image/png" })
-                    return item;
-                })
-                console.log(relatedProduct)
-                setRelatedProduct(relatedProducts);
-            })
+            setProduct({...data, images});
+            setProductImage(images[0])
         }
 
 
-    }, [id])
+    }, [data])
 
     const addToBasket = () => {
         const index = basket.findIndex(item => {
@@ -95,7 +71,7 @@ const ProductDetails = () => {
     }
 
     const addToFavorite = () => {
-        fetch(ApiUrls.favorites.addFavorite, {
+        fetch(ApiUrls.hostName+ApiUrls.favorites.addFavorite, {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
@@ -108,52 +84,52 @@ const ProductDetails = () => {
                     position: 'bottom-right'
                 });
             }
-        })
+        }).catch(error => console.log(error))
     }
 
-    function slideTo(direction) {
-        console.dir(sliderItemRef.current)
-        const cardWidth = sliderItemRef.current.offsetWidth + 20;
-        if (direction === "left") {
-            sliderRef.current.scroll(sliderRef.current.scrollLeft - cardWidth, 0)
-        } else if (direction === "right") {
-            sliderRef.current.scroll(sliderRef.current.scrollLeft + cardWidth, 0)
-        }
-    }
-
-    function modalSlide(previosImage, direction){
+    function modalSlide(previosImage, direction) {
         const index = product?.images.findIndex(item => {
             return item == previosImage
         })
-        switch(direction){
+        switch (direction) {
             case 'left':
-                setProductImage(product.images[index - 1 < 0  ? product.images.length-1 : index-1]);
+                setProductImage(product.images[index - 1 < 0 ? product.images.length - 1 : index - 1]);
                 break;
             case 'right':
-                setProductImage(product.images[index + 1 == product.images.length ? 0 : index+1]);
+                setProductImage(product.images[index + 1 == product.images.length ? 0 : index + 1]);
                 break
         }
-        
+
+    }
+
+    //if data is loading from server
+    if (loading) {
+        productsElement = (
+            <Loading />
+        )
+    }
+
+    if(error){
+        productsElement = <NotFound/>
     }
 
     //if data have been loaded from server
     if (product) {
-        producstElement = (
+        productsElement = (
             <div className="fade-in">
-
                 <div className="product_details">
                     <section className='product_images'>
                         <Button btnType="transparent zoom-btn" click={() => setShowModal(!showModal)} ><i className='bi bi-zoom-in'></i></Button>
-                        <img src={productImage} className="product_image" alt={product.name} />
+                        <img src={productImage} className="product_image" alt={product?.name} />
                         <Modal show={showModal} ModalClose={() => setShowModal(!showModal)}>
                             <span className='slide-left' onClick={() => modalSlide(productImage, 'left')}> <i className='bi bi-chevron-left'></i></span>
-                            <img src={productImage} alt={product.name} />
+                            <img src={productImage} alt={product?.name} />
                             <span className='slide-left right' onClick={() => modalSlide(productImage, 'right')}><i className='bi bi-chevron-right'></i></span>
                         </Modal>
                         <div className='different_sides align_center'>
-                            {product.images.map((item) => {
+                            {product?.images.map((item) => {
                                 return (
-                                    <div className={'image_side'+ (item == productImage ? ' active': '')} onClick={()=> setProductImage(item)}>
+                                    <div className={'image_side' + (item == productImage ? ' active' : '')} onClick={() => setProductImage(item)}>
                                         <img src={item} alt={product.name} />
                                     </div>
                                 )
@@ -195,41 +171,16 @@ const ProductDetails = () => {
                         <DetailsPane dataSheet={product} description={product.description} />
                     </div>
                 </div>
-                <section className='related_products'>
-                    <h2 className='section-title'>
-                        <span style={{ color: "var(--mainColor)" }}>Related</span> Products
-                    </h2>
-                    <div className='related_product_container' >
-                        <span className='scroll_btn' onClick={() => slideTo("left")}><i className='bi bi-chevron-left'></i></span>
-                        <div className='related_product_slider' ref={sliderRef} draggable>
-                            {relatedProduct.map(item => {
-                                return <Product
-                                    id={item.productId}
-                                    image={item.images[0]}
-                                    name={item.name}
-                                    price={item.price}
-                                    rating={item?.rate}
-                                    key={item.productId}
-                                    color={item.color}
-                                    discount={item.discount}
-                                    customeRef={sliderItemRef}
-                                />
-                            })}
-                        </div>
-
-                        <span className='scroll_btn right' onClick={() => slideTo("right")}><i className='bi bi-chevron-right'></i></span>
-                    </div>
-                </section>
-
+                <RelatedProducts category={product?.category} />
             </div>
         )
-    } else {
-        //if data have not been loaded from server
-        producstElement = (
-            <Loading />
-        )
+        
     }
-    return producstElement
+
+    return (
+        <div>{productsElement}</div>
+    )
+    
 
 }
 
