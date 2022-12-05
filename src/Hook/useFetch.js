2466dec627859a01c8f90162d558c183cefeacb7
Aliherawi7 <Aliherawi7@gmail.com>
2022-12-05 13:55:37 +0430

    useFetch hook implemented to decrease dirty codes in useEffect hook in all components

--- src/Hook/useFetch.js
+++ src/Hook/useFetch.js
@@ -1,76 +1,40 @@
-import React, { useReducer, useRef, useEffect } from 'react';
+import React, { useReducer, useState, useEffect } from 'react';
 
 const actions = {
     LOADING: "loading",
     FETCHED: "fetched",
     ERROR: "error"
 }
-
-let Cache = {}
-
-function useFetch(url, options) {
-    const cache = useRef({});
-    const initialState = {
-        data: undefined,
-        error: undefined,
-        status: ""
-    }
-    const [state, dispatch] = useReducer(fetchReducer, initialState);
-
-    // the logic for updating the state
-    const fetchReducer = (state, action) => {
-        switch (action) {
-            case actions.LOADING:
-                return { ...initialState, state: actions.LOADING }
-            case actions.FETCHED:
-                return { ...initialState, data: action.payload, state: actions.FETCHED }
-            case actions.ERROR:
-                return { ...initialState, error: action.payload, state: actions.ERROR }
-            default:
-                return state;
-        }
-    }
+function useFetch(url, options){
+    const [data, setData] = useState();
+    const [error, setError] = useState();
+    const [loading, setLoading] = useState();
 
     useEffect(() => {
-        // if url is null or undefined then do nothing
-        if (!url) return;
+      (async function(){
+          try{
+              setLoading(true);
+              const response = await fetch(url,options);
+              if(!response.ok){
+                  throw new Error(response.statusText);
+              }
+              if(response.ok && response.status >= 300){
+                throw new Error("something went wrong, you may forgot something")
+              }
 
-        const getData = async () => {
-            dispatch({ type: actions.LOADING });
-
-            // if a cache is exist for this url
-
-            // ---------------
-
-            try {
-                const response = await fetch(url, options);
-                if (!response.ok) {
-                    throw new Error(response.statusText);
-                }
-                if (response.ok && response.status != 200) {
-                    throw new Error("302 error happen. Maybe you forgat something");
-                }
-
-                const data = await response.json();
-
-                cache.current[url] = data
-                dispatch({ type: actions.FETCHED, payload: data })
-
-            } catch (error) {
-                dispatch({
-                    type: actions.ERROR,
-                    payload: error
-                })
-            }
-
-        }
-
-        getData();
-
+              const responseData = await response.json();
+              setData(responseData)
+          }catch(error){
+              setError(error)
+          }finally{
+              setLoading(false)
+          }
+      })()
+    
     }, [url])
 
-    return state;
-
+    return {data, error, loading}
 }
+
 
 export default useFetch;
