e65428d38f90416fa755c4e1c0562eb8a020035d
Aliherawi7 <Aliherawi7@gmail.com>
2022-12-08 13:26:08 +0430

    code formatted and if block added for condition if the request status is 204

--- src/Hook/useFetch.js
+++ src/Hook/useFetch.js
@@ -5,35 +5,39 @@ const actions = {
     FETCHED: "fetched",
     ERROR: "error"
 }
-function useFetch(url, options){
+function useFetch(url, options) {
     const [data, setData] = useState();
     const [error, setError] = useState();
     const [loading, setLoading] = useState();
 
     useEffect(() => {
-      (async function(){
-          try{
-              setLoading(true);
-              const response = await fetch(url,options);
-              if(!response.ok){
-                  throw new Error(response.statusText);
-              }
-              if(response.ok && response.status >= 300){
-                throw new Error("something went wrong, you may forgot something")
-              }
+        (async function () {
+            try {
+                setLoading(true);
+                const response = await fetch(url, options);
+                if (!response.ok) {
+                    throw new Error(response.statusText);
+                }
+                if (response.ok && response.status >= 300) {
+                    throw new Error("something went wrong, you may forgot something")
+                }
+                if (response.ok && response.status == 204) {
+                    setData([])
+                    return;
+                }
 
-              const responseData = await response.json();
-              setData(responseData)
-          }catch(error){
-              setError(error)
-          }finally{
-              setLoading(false)
-          }
-      })()
-    
+                const responseData = await response.json();
+                setData(responseData)
+            } catch (error) {
+                setError(error)
+            } finally {
+                setLoading(false)
+            }
+        })()
+
     }, [url])
 
-    return {data, error, loading}
+    return { data, error, loading, setData }
 }
 
 
