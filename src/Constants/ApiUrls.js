bf0e091d29d74b64f911a032d94ecfbf82ec5cd8
ali herawi <aliherawi7@gmail.com>
2023-04-02 01:09:33 +0430

    new endpoints added

--- src/Constants/ApiUrls.js
+++ src/Constants/ApiUrls.js
@@ -1,27 +1,31 @@
 
 const ApiUrls = {
-    hostName : "http://localhost:8080",
+    hostName: "http://localhost:8080/",
     products: {
-        allProducts: "/api/products",
-        getProduct: "/api/products/",
-        addProduct: "/api/products",
-        deleteProduct: "/api/products/delete/",
-        findProducts: "/api/products/find?"
+        allProducts: "api/products",
+        getProduct: "api/products/",
+        addProduct: "api/products",
+        deleteProduct: "api/products/delete/",
+        findProducts: "api/products/find?"
     },
     users: {
-        getUsers: "/api/users",
-        putUser: "/api/users",
-        getUser: "/api/users/user",
-        addUser: "/api/users/signup",
-        addRoleToUser: "/api/users/addRoleToUser"
+        getUsers: "api/users",
+        putUser: "api/users",
+        getUser: "api/users/user",
+        addUser: "api/users/signup",
+        addRoleToUser: "api/users/addRoleToUser"
     },
-    favorites:{
-        allFavorites:"/api/favorites",
-        addFavorite:"/api/favorites"
+    favorites: {
+        allFavorites: "api/favorites",
+        addFavorite: "api/favorites"
 
     },
-    comments:{
-        productComments:'/api/comments/products/',  
+    comments: {
+        productComments: 'api/comments/products/',
+    },
+    files: {
+        productImage: (productId, side) => `api/v1/files/product-image/${productId}/${side}`,
+        userImage: (userId) => `api/v1/files/user-image/${userId}`
     }
 }
 export default ApiUrls
